67363438113382724f834ba0e617988c4efd47a2
Umut Şengöl <umutsengol@outlook.com>
2026-04-22 16:22:13 +0300

    Update UpdateTahsilatYetkiRequest.php

--- kaynak/app/Http/Requests/UpdateTahsilatYetkiRequest.php
+++ kaynak/app/Http/Requests/UpdateTahsilatYetkiRequest.php
@@ -21,6 +21,10 @@ class UpdateTahsilatYetkiRequest extends FormRequest
             'tahsilat_takip_sorumlusu' => ['required', 'boolean'],
             'aktif' => ['required', 'boolean'],
             'tab_permissions' => ['nullable', 'array'],
+
+            // YENİ EKLENECEK KISIM:
+            'sorumlu_muvekkiller' => ['nullable', 'array'],
+            'sorumlu_muvekkiller.*' => ['integer'],
         ];
     }
 }
